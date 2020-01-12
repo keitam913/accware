c0614776132ad85843a790a8585d0a6c8a505341
keitam <keitam913@yahoo.co.jp>
2020-01-12 16:51:02 +0900

    Add the config reader

---
+++ infrastructure/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Persons []Person `yaml:"persons"`
+}
+
+type Person struct {
+	ID    string  `yaml:"id"`
+	Name  string  `yaml:"name"`
+	Ratio float64 `yaml:"ratio"`
+}
+
+func Load(filepath string) (*Config, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var conf Config
+	if err := yaml.NewDecoder(f).Decode(&conf); err != nil {
+		return nil, err
+	}
+	return &conf, nil
+}
---
+++ infrastructure/config/person_service.go
@@ -0,0 +1,24 @@
+package config
+
+import "github.com/keitam913/accware-api/domain/person"
+
+type PersonService struct {
+	Config *Config
+}
+
+func (ps *PersonService) PaymentRatio(personID string) (float64, error) {
+	for _, person := range ps.Config.Persons {
+		if personID == personID {
+			return person.Ratio, nil
+		}
+	}
+	return 0, nil
+}
+
+func (ps *PersonService) Persons() ([]person.Person, error) {
+	var p []person.Person
+	for _, cp := range ps.Config.Persons {
+		p = append(p, person.New(cp.ID, cp.Name))
+	}
+	return p, nil
+}
