792bbb53f7c5ce5e37dbf60c7bc15ea9cc0fd088
keitam <keitam913@yahoo.co.jp>
2020-01-12 16:54:22 +0900

    Inject components

--- di/container.go
+++ di/container.go
@@ -1,7 +1,13 @@
 package di
 
 import (
-	"net/http"
+	accountapp "github.com/keitam913/accware-api/application/account"
+	"github.com/keitam913/accware-api/domain/account"
+	"github.com/keitam913/accware-api/domain/person"
+	"github.com/keitam913/accware-api/infrastructure/config"
+	"github.com/keitam913/accware-api/infrastructure/rest"
+
+	"github.com/keitam913/accware-api/infrastructure/oidc"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +15,40 @@ import (
 type Container struct{}
 
 func (c Container) Router() *gin.Engine {
-	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "hello")
-	})
-	return r
+	return rest.NewRouter(c.OIDCService(), c.MonthHandler())
+}
+
+func (c Container) MonthHandler() *rest.MonthHandler {
+	return &rest.MonthHandler{
+		Service:       c.AccountApplicationService(),
+		PersonService: c.PersonService(),
+	}
+}
+
+func (c Container) AccountApplicationService() *accountapp.Service {
+	return &accountapp.Service{
+		Repository: c.AccountRepository(),
+	}
+}
+
+func (c Container) AccountRepository() account.Respository {
+	return nil
+}
+
+func (c Container) OIDCService() *oidc.Service {
+	return &oidc.Service{}
+}
+
+func (c Container) PersonService() person.Service {
+	return &config.PersonService{
+		Config: c.Config(),
+	}
+}
+
+func (c Container) Config() *config.Config {
+	conf, err := config.Load("config.dev.yaml")
+	if err != nil {
+		panic(err)
+	}
+	return conf
 }
