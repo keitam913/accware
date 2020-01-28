d0071f05671a80975b8304228e28c3744029c4c5
keitam <keitam913@yahoo.co.jp>
2020-01-28 22:29:38 +0900

    Response dates without time

--- rest/month_handler.go
+++ rest/month_handler.go
@@ -52,7 +52,7 @@ func (mc *MonthHandler) makeResponse(monthAccount account.Month) (Month, error) 
 			Name:     i.Name(),
 			Amount:   i.Amount(),
 			PersonID: i.PersonID(),
-			Date:     i.Date().Format("2006-01-02 15:04:05"),
+			Date:     i.Date().Format("2006-01-02"),
 		})
 	}
 	as := []Amount{}
