d307e2df9bf333ff64ea64a396d0c6bde00e4bb1
keitam <keitam913@yahoo.co.jp>
2020-04-12 20:13:05 +0900

    Fix the routing rule

--- rest/router.go
+++ rest/router.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/gin-contrib/cors"
 	"github.com/keitam913/accware/api/oidc"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +12,6 @@ import (
 
 func AuthMiddleware(idService *oidc.Service) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		if !strings.HasPrefix(ctx.Request.URL.Path, "/v") {
-			ctx.Next()
-			return
-		}
 		id, err := idService.Authenticate(ctx.Request.Header.Get("ID-TOKEN"))
 		if err != nil {
 			if errors.Is(err, oidc.InvalidToken) {
@@ -27,45 +22,39 @@ func AuthMiddleware(idService *oidc.Service) gin.HandlerFunc {
 			return
 		}
 		ctx.Set("id", id)
-		ctx.Next()
 	}
 }
 
 func NewRouter(idService *oidc.Service, monthHandler *MonthHandler, itemHandler *ItemHandler) *gin.Engine {
-	r := gin.Default()
-
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"https://localhost:3000",
-			"https://accware.keitam.com",
-		},
-		AllowHeaders: []string{
-			"ID-Token",
-		},
-		AllowMethods: []string{"GET", "POST", "DELETE"},
-	}))
-
-	r.Use(AuthMiddleware(idService))
-
-	v1r := gin.Default()
+	v1r := gin.New()
+	v1r.Use(AuthMiddleware(idService))
 	v1 := v1r.Group("/v1")
 	v1.GET("/accounts/:year/:month", monthHandler.Get)
 	v1.POST("/items", itemHandler.Post)
 	v1.DELETE("/items/:id", itemHandler.Delete)
 
-	sr := gin.Default()
+	sr := gin.New()
 	sr.Static("/static", "assets/static")
 
-	r.GET("/*resource", func(ctx *gin.Context) {
+	tr := gin.New()
+	tr.Any("/*resource", func(ctx *gin.Context) {
+		ctx.File("assets/index.html")
+	})
+
+	r := gin.Default()
+
+	r.Any("/*resource", func(ctx *gin.Context) {
 		if strings.HasPrefix(ctx.Param("resource"), "/v1") {
-			v1r.HandleContext(ctx)
+			v1r.ServeHTTP(ctx.Writer, ctx.Request)
 			return
 		}
 		if strings.HasPrefix(ctx.Param("resource"), "/static") {
-			sr.HandleContext(ctx)
+			sr.ServeHTTP(ctx.Writer, ctx.Request)
 			return
 		}
-		ctx.File("assets/index.html")
+		tr.ServeHTTP(ctx.Writer, ctx.Request)
+		tr.HandleContext(ctx)
+		return
 	})
 
 	return r
